517196250a8ab6d574ff999c0fd49b8a678b1af6
Fernando Neto <cabralneto2004@gmail.com>
2023-08-20 22:16:19 +0000

    correcao funcao nomes e add funcao incorporacao

--- BaralhoT10_2023/src/App.java
+++ BaralhoT10_2023/src/App.java
@@ -1,11 +1,20 @@
 import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
+        Random ra = new Random();
+
         //Cria os jogadores e da os nomes
         Scanner sc = new Scanner(System.in);
         System.out.println("Digite o nome do Jogador 1 e em seguida o nome do Jogador 2");
         String nomeJogador1 = sc.next();
         String nomeJogador2 = sc.next();
+        Player player1 = new Player(nomeJogador1);
+        Player player2 = new Player(nomeJogador2);
+        String p1 = player1.getNome();
+        String p2 = player2.getNome();
+
+        System.out.println(player1 + " CONTRA " + player2);
+
         sc.close();
       
         
@@ -15,6 +24,7 @@ public class App {
         // Cria os decks dos jogadores
         Deck jogador1 = new Deck();
         Deck jogador2 = new Deck();
+        Deck empate = new Deck();
         // Distribui todas as cartas entre os dois jogadore
         while(!baralho.vazio()){
             jogador1.insereEmbaixo(baralho.retiraDeCima());
@@ -28,24 +38,38 @@ public class App {
             Carta cj1 = jogador1.retiraDeCima();
             Carta cj2 = jogador2.retiraDeCima();
             System.out.println("Rodada: "+rodada);
-            System.out.println("Carta do "+nomeJogador1 +": "+cj1.toString());
-            System.out.println("Carta do "+nomeJogador2 +": "+cj2.toString());
+            System.out.println("Carta do "+player1 +": "+cj1.toString());
+            System.out.println("Carta do "+player2 +": "+cj2.toString());
+
+            boolean ordemaleatoria = ra.nextBoolean();
         
             // Se a carta do jogador1 é maior, ele fica com todas
             if (cj1.eMaior(cj2)){
+                if(ordemaleatoria){
                 jogador1.insereEmbaixo(cj1);
                 jogador1.insereEmbaixo(cj2);
-                System.out.println(nomeJogador1 + " ganhou a rodada");
+            }
+                else{
+                    jogador2.insereEmbaixo(cj2);
+                    jogador2.insereEmbaixo(cj1); 
+                }
+                System.out.println(p1 + " ganhou a rodada");
             }else{
+                if(ordemaleatoria){
                 jogador2.insereEmbaixo(cj2);
                 jogador2.insereEmbaixo(cj1);
-                System.out.println(nomeJogador2 +" ganhou a rodada");
+            }
+                else{
+                    jogador1.insereEmbaixo(cj1);
+                    jogador1.insereEmbaixo(cj2);
+                }
+                System.out.println(p2 +" ganhou a rodada");
             }
             // Verifica se acabou
             if (jogador1.vazio() || jogador2.vazio()){
                 acabou = true;
-                if(jogador1.vazio()) System.out.println(nomeJogador2 + " GANHOU!");
-                else System.out.println(nomeJogador1 + " GANHOU!");
+                if(jogador1.vazio()) System.out.println(p2 + " GANHOU!");
+                else System.out.println(p1 + " GANHOU!");
             }
             // Incrementa a rodada
             rodada++;
