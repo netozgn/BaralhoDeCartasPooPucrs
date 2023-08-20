3e346c41f2a239329ef73514de9832ff4eb6a91f
Fernando Neto <cabralneto2004@gmail.com>
2023-08-20 21:07:25 +0000

    adicionada funcoes dos nomes e ganhador

--- BaralhoT10_2023/src/App.java
+++ BaralhoT10_2023/src/App.java
@@ -1,5 +1,14 @@
+import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
+        //Cria os jogadores e da os nomes
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do Jogador 1 e em seguida o nome do Jogador 2");
+        String nomeJogador1 = sc.next();
+        String nomeJogador2 = sc.next();
+        sc.close();
+      
+        
         // Cria o baralho e embaralha
         Baralho baralho = new Baralho();
         baralho.embaralha();
@@ -19,21 +28,24 @@ public class App {
             Carta cj1 = jogador1.retiraDeCima();
             Carta cj2 = jogador2.retiraDeCima();
             System.out.println("Rodada: "+rodada);
-            System.out.println("Carta do jogador 1: "+cj1.toString());
-            System.out.println("Carta do jogador 2: "+cj2.toString());
+            System.out.println("Carta do "+nomeJogador1 +": "+cj1.toString());
+            System.out.println("Carta do "+nomeJogador2 +": "+cj2.toString());
+        
             // Se a carta do jogador1 é maior, ele fica com todas
             if (cj1.eMaior(cj2)){
                 jogador1.insereEmbaixo(cj1);
                 jogador1.insereEmbaixo(cj2);
-                System.out.println("Jogador 1 ganhou a rodada");
+                System.out.println(nomeJogador1 + " ganhou a rodada");
             }else{
                 jogador2.insereEmbaixo(cj2);
                 jogador2.insereEmbaixo(cj1);
-                System.out.println("Jogador 2 ganhou a rodada");
+                System.out.println(nomeJogador2 +" ganhou a rodada");
             }
             // Verifica se acabou
             if (jogador1.vazio() || jogador2.vazio()){
                 acabou = true;
+                if(jogador1.vazio()) System.out.println(nomeJogador2 + " GANHOU!");
+                else System.out.println(nomeJogador1 + " GANHOU!");
             }
             // Incrementa a rodada
             rodada++;
@@ -41,3 +53,4 @@ public class App {
         System.out.println("Fim");
     }
 }
+
---
+++ BaralhoT10_2023/src/Player.java
@@ -0,0 +1,9 @@
+public class Player {
+    private String nome;
+    public Player(String nome){
+        this.nome = nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+}
